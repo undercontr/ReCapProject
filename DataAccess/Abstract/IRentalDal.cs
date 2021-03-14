6a13b9f62d0b89931f32f264e2421817e6190318
undercontr <onder.alkan15@gmail.com>
2021-03-14 23:47:23 +0300

    Customer, User and Rental entities added.

---
+++ DataAccess/Abstract/IRentalDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.EntityFramework;
+using Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+    public interface IRentalDal : IEntityRepository<Rental>
+    {
+        
+    }
+}
---
+++ DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+using Core.EntityFramework;
+using Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserDal : IEntityRepository<User>
+    {
+    }
+}
